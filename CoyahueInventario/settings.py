db89d3da8479bc3d8d401a1329f022961d79016e
MoisesBascur <moises.bascur@inacapmail.cl>
2025-12-10 03:11:08 +0000

    Guardando configuracion de servidor S3 y DB

--- CoyahueInventario/settings.py
+++ CoyahueInventario/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-7ojj32p58yj5%e(#q41+%2a(p2$o+yo%h1fnz&p1@z^(^lxs^6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.55.43.210','coyahueinventario.duckdns.org']
 
 
 # Application definition
@@ -81,8 +83,12 @@ WSGI_APPLICATION = 'CoyahueInventario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'coyahue',
+        'USER':'coyahue_user',
+        'PASSWORD':'CONtraseña4321',
+        'HOST':'172.31.23.251',
+        'PORT':'5432',
     }
 }
 
@@ -186,26 +192,5 @@ MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 # Esto evita que sobreescriba archivos con el mismo nombre
 AWS_S3_FILE_OVERWRITE = True
 
-# ===================== Nuevo ==============================
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
 
-import os
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-# 2. Le decimos a Django que use el disco duro local, NO S3
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod')
